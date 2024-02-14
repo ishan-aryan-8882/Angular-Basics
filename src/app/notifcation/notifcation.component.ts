6f425bad3e834263c6f814385df9b16facdfd76f
unknown <Devl-Minhaj>
2024-02-14 14:35:49 +0530

    first commit

---
+++ src/app/notifcation/notifcation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notifcation',
+  template: '<div><p><i class="fa-solid fa-chevron-left"></i> This website uses cookies for better experience</p></div> ',
+  styleUrls: ['./notifcation.component.css']
+})
+export class NotifcationComponent {
+
+}
